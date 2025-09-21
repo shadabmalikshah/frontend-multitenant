e6accd36c8246b443bd712b8cd8185a7c73332c2
shadabmalikshah <97905885+shadabmalikshah@users.noreply.github.com>
2025-09-21 17:02:24 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Multitenant POC brand', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const brandElement = screen.getByText(/Multitenant POC/i);
+  expect(brandElement).toBeInTheDocument();
 });
